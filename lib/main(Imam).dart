a608ee82f90a05c2d7edb48480902ade2a42aa3f
ImamRasy <95610500+ImamRasy@users.noreply.github.com>
2022-06-09 20:51:38 +0700

    Create main(Imam).dart

---
+++ lib/main(Imam).dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Oleh Shop',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.orange,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final List<Map> myProducts =
+        List.generate(10, (index) => {"name": "Product $index"}).toList();
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[50],
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.menu,
+                    size: 50,
+                  ),
+                  Text(
+                    'OLEH SHOP',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  Icon(
+                    Icons.account_circle_outlined,
+                    size: 50,
+                  ),
+                ],
+              ),
+              Container(
+                width: 500.0,
+                height: 25.0,
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  border: Border.all(color: Colors.grey, width: 3),
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  child: Text("ID Pemesanan: #ID_Pemesanan",
+                      style: TextStyle(fontSize: 10)),
+              ),
+              Container(
+                width: 500.0,
+                height: 25.0,
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  border: Border.all(color: Colors.grey, width: 3),
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  child: Text("No Pembayaran: #no_pembayaran",
+                      style: TextStyle(fontSize: 10)),
+              ),
+              Container(
+                width: 500.0,
+                height: 25.0,
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  border: Border.all(color: Colors.grey, width: 3),
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  child: Text("Tanggal Pemesanan: #Tanggal_Pemesanan",
+                      style: TextStyle(fontSize: 10)),
+              ),
+              Container(
+                width: 500.0,
+                height: 25.0,
+                margin: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  border: Border.all(color: Colors.grey, width: 3),
+                  borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  child: Text("Bukti Pembayaran: #Bukti_Pembayaran",
+                      style: TextStyle(fontSize: 10)),
+              ),
+              Text("Total: "),
+              ElevatedButton(
+                onPressed: () {
+                  // Respond to button press
+                },
+                child: Text('BAYAR'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
