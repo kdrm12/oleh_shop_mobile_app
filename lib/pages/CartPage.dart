ef75e2152d659b00979f6b0b1cb1a8610f11f4f4
Kevin <96371699+kdrm12@users.noreply.github.com>
2022-06-25 12:26:48 +0700

    Update keranjang

--- lib/pages/CartPage.dart
+++ lib/pages/CartPage.dart
@@ -9,27 +9,70 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-  final List<String> entries = <String>['A', 'B', 'C'];
-  final List<int> colorCodes = <int>[600, 500, 100];
-
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    var padding = MediaQuery.of(context).padding;
+    var newheight = (height - padding.top - padding.bottom - kToolbarHeight);
     return Scaffold(
       backgroundColor: Colors.blueGrey[50],
       appBar: AppBar(title: const Text("Keranjang")),
       drawer: drawer(context),
-      body: SafeArea(
-        child: ListView.builder(
-          padding: const EdgeInsets.all(8),
-          itemCount: entries.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: 50,
-              color: Colors.amber[colorCodes[index]],
-              child: Center(child: Text('Entry ${entries[index]}')),
-            );
-          },
-        ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: newheight * 0.9,
+            child: SafeArea(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: 10,
+                itemBuilder: (BuildContext context, int index) {
+                  return const Card(
+                    child: ListTile(
+                      leading: FlutterLogo(size: 56.0),
+                      title: Text('Lorem Ipsum'),
+                      subtitle: Text('Rp15000 (2x)'),
+                      trailing: Icon(Icons.delete),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          Container(
+            height: newheight * 0.1,
+            color: Colors.orange.shade100,
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Total : Rp20000",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: newheight * 0.1,
+                    width: width * 0.2,
+                    color: Colors.green,
+                    child: const Center(
+                        child: Text(
+                      "Order",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    )),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -39,101 +39,55 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SafeArea(
         child: Container(
           margin: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //   crossAxisAlignment: CrossAxisAlignment.center,
-              //   children: const [
-              //     Icon(
-              //       Icons.menu,
-              //       size: 50,
-              //     ),
-              //     Text(
-              //       'OLEH SHOP',
-              //       style: TextStyle(
-              //         fontSize: 40,
-              //         fontWeight: FontWeight.w900,
-              //         color: Colors.black87,
-              //       ),
-              //     ),
-              //     Icon(
-              //       Icons.account_circle_outlined,
-              //       size: 50,
-              //     ),
-              //   ],
-              // ),
-              // Container(
-              //   height: 42,
-              //   margin: const EdgeInsets.all(10),
-              //   decoration: BoxDecoration(
-              //     borderRadius: BorderRadius.circular(12),
-              //     color: Colors.white,
-              //     border: Border.all(color: Colors.black26),
-              //   ),
-              //   padding: const EdgeInsets.symmetric(horizontal: 8),
-              //   child: const TextField(
-              //     decoration: InputDecoration(
-              //         icon: Icon(Icons.search), border: InputBorder.none),
-              //   ),
-              // ),
-              SizedBox(
-                height: 500,
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    childAspectRatio: 3 / 2,
-                    crossAxisSpacing: 20,
-                    mainAxisSpacing: 20,
-                  ),
-                  itemCount: _oleh.length,
-                  itemBuilder: (BuildContext ctx, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(ctx, '/detail',
-                            arguments: _oleh[index]);
-                      },
-                      child: Container(
-                        alignment: Alignment.center,
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 90,
-                              width: 185,
-                              child: CachedNetworkImage(
-                                imageUrl: 'http://10.0.2.2:8000/storage/' +
-                                    _oleh[index].gambar,
-                                //width: 80,
-                                fit: BoxFit.fill,
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.all(5),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    _oleh[index].nama,
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18),
-                                  ),
-                                  Text("Rp" + _oleh[index].harga.toString()),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                        decoration: const BoxDecoration(
-                          color: Colors.amber,
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+            ),
+            itemCount: _oleh.length,
+            itemBuilder: (BuildContext ctx, index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(ctx, '/detail', arguments: _oleh[index]);
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 90,
+                        width: 185,
+                        child: CachedNetworkImage(
+                          imageUrl: 'http://10.0.2.2:8000/storage/' +
+                              _oleh[index].gambar,
+                          //width: 80,
+                          fit: BoxFit.fill,
                         ),
                       ),
-                    );
-                  },
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              _oleh[index].nama,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                            Text("Rp" + _oleh[index].harga.toString()),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  decoration: const BoxDecoration(
+                    color: Colors.amber,
+                  ),
                 ),
-              ),
-            ],
+              );
+            },
           ),
         ),
       ),
